d306f8dbfef1fc35625eaee57973640c7870eeef
James Stevens <jtg.stevens@outlook.com>
2021-08-23 20:22:45 +0100

    Handle arrow keys and backspace/delete

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ function Square(props) {
     <td 
       className={classname} 
       onClick={props.onClick} 
-      onKeyPress={(e) => props.onKeyPress(e)}
-      // tabIndex ensures onKeyPress works
+      onKeyDown={(e) => props.onKeyDown(e)}
+      // tabIndex ensures onKeyDown works
       tabIndex="0"
+      id={"cell" + props.cell.index} 
     >
       {props.cell.value !== 0 ? props.cell.value : ''}
     </td>
@@ -113,40 +114,68 @@ class SudokuApp extends React.Component {
     // finally, set the selected cell to be selected
     board[theRow][theCol].selected = !board[theRow][theCol].selected;
 
-
     this.setState({ board });
   }
 
-  handleKeyPress(event, index) {
-    console.log("cell #" + index + ", keycode " + event.charCode);
+  handleKeyDown(event, index) {
+    console.log("cell #" + index + ", keycode " + event.keyCode);
 
     let board = this.state.board.slice();
     let theRow = Math.floor(index / 9);
     let theCol = index % 9;
     let cell = board[theRow][theCol];
 
-    if (cell.fixed) {
+    // Switch on keycode
+    let keycode = event.keyCode;
+    if (keycode >= 37 && keycode <= 40) {
+      // 37 - L, 38 - U, 39 - R, 40 - D
+      if (keycode === 37) {
+        if (theCol === 0) return;
+        index -= 1;
+      }
+      else if (keycode === 38) {
+        if (theRow === 0) return;
+        index -= 9;
+      }
+      else if (keycode === 39) {
+        if (theCol === 8) return;
+        index +=1;
+      }
+      else {
+        if (theRow === 8) return;
+        index +=9;
+      }
+
+      let newCell = document.getElementById("cell" + index)
+      // Need to give the next cell focus so that the keyboard continues to work
+      newCell.focus();
+      // Calling click() will simulate a click which will be handled as usual
+      newCell.click();
+    }
+    else if ((keycode >= 48 && keycode <= 57) || keycode === 8 || keycode === 46) {
+      // 0-9 is 48-57
+      // 8: backspace; 46: delete - both should empty the current cell
+      if (cell.fixed) {
+        return;
+      }
+      
+      let digit = keycode - 48; 
+      if (digit < 0) digit = 0;
+      cell.value = digit;
+      this.setState({ board });
+    }
+    else {
       return;
     }
-
-    // charcodes: 0-9 is 48-57
-    let charcode = event.charCode;
-    if (charcode > 57 || charcode < 48) {
-      return;
-    }
-
-    let digit = charcode - 48; 
-    cell.value = digit;
-
-    this.setState({ board });
   }
 
   renderSquare(cell) {
-    return <Square key={cell.index} cell={cell} onClick={() => this.handleClick(cell.index)} onKeyPress={(e) => this.handleKeyPress(e, cell.index)} />;
+    return <Square key={cell.index} cell={cell} onClick={() => this.handleClick(cell.index)} onKeyDown={(e) => this.handleKeyDown(e, cell.index)} />;
   }
 
   render(){
     console.log(this.state);
+    let ix = 0;
     if (this.state && this.state.board) {
       return (
         <div className="SudokuApp">
@@ -154,7 +183,7 @@ class SudokuApp extends React.Component {
             {
               this.state.board.map((row) => {
                 return (
-                  <tr>
+                  <tr key={ix++}>
                     { 
                       row.map((cell) => {
                         return this.renderSquare(cell);
